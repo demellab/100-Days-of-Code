d28f8f654b94996d579cdbe4f3869c443b830841
demellab <demellab@sheridancollege.ca>
2025-05-31 18:46:59 -0400

    first coomit

---
+++ Day 1/task/task.py
@@ -0,0 +1,6 @@
+print("Hello to Band Name generator!")
+city = input("What city did you grow up in?\n")
+
+pet=input("What is the name of your first pet?\n")
+
+print("The name of your band is:", city, pet)
