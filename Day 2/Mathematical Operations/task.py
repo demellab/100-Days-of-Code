7c5947262ac335bcea925c7c09c440dad38856fb
Your Name <demellab@sheridancollege.ca>
2025-06-02 13:52:55 -0400

    Number Manipulations

--- Day 2/Mathematical Operations/task.py
+++ Day 2/Mathematical Operations/task.py
@@ -1,1 +1,14 @@
 print("My age: " + str(12))
+print (39+1)
+print (7-3)
+print(3*2)
+print(6/3) #float answer - implicit type casting
+print (6//3) #integer , it removes all of the decimal places
+print(5//3)
+print(4**2) #exponent
+
+#PEMDAS - parenthesis, Exponent, Multiply, Divide, Addition, Subtraction
+# calculation goes LR
+
+#change the code to output 3
+print(3 * (3 + 3) // 3 - 3)
--- Day 2/Number Manipulation/task.py
+++ Day 2/Number Manipulation/task.py
@@ -1,1 +1,24 @@
 bmi = 84 / 1.65 ** 2
+print(bmi)
+
+print(int(bmi)) #flooring the number to the lowest whole number
+
+print(round(bmi)) #will round it to the closest whole number
+
+print(round(bmi,3))
+
+#assignment operators
+
+score = 3
+score += 1
+print(score)
+
+#F strings
+print("Your score is: ", score)
+
+#example of F Strings
+score = 3
+height = 104.4
+weight = 45
+
+print(f"Your score is = {score}, your height is: {104.4}, your weight is: {weight}")
