21efaf283e9368e34c3954c9cb260cfbcdabdd4f
Your Name <demellab@sheridancollege.ca>
2025-06-02 12:45:18 -0400

    Type Errors & Conversions

--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,1 +1,25 @@
-len("Hello heyy check!! ")
+print("Hello"[1]) #this is subscripting : Pulling out a specific characters from a string
+print("hello"[4])
+
+#using negative indices
+print("HELLO"[-1])
+
+#once a character is within "" it is treated like a string
+print("124" + "356") #concatenation, won't do a add
+
+#Integer: WHOLE NUMBERS/no decimals
+print(123 + 345)
+
+#Large Integers: we use commas to seperate IRL, but in Python you can use _
+
+print(123_428_2920)
+
+#Float : numbers with decimals
+print (3.419)
+
+#Boolean
+print(True)
+print(False)
+
+# "False" is a string
+# False is a Boolean
--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,18 @@
-len(12345)
+
+#printing all four data types
+print(type("abira demello"))
+print(type(1267))
+print(type(3.47))
+print(type(True))
+
+#type casting
+print(int("12") + int("1"))
+
+int()
+float()
+str()
+bool()
+
+#print("Number of letters in your name: " + str(len(input("Enter your name")))
+
+print("Number of letters are:" + str(len(input("Enter you name here:"))))
