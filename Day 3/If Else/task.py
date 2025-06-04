e91c8b5af4627b26450d348a08e4064d68118b34
Your Name <demellab@sheridancollege.ca>
2025-06-04 12:11:26 -0400

    Control Flow

--- Day 3/If Else/task.py
+++ Day 3/If Else/task.py
@@ -1,2 +1,10 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+
+if height > 120:
+    print("You can ride, Welcome!!")
+else:
+    print("Sorry, not this time!")
+
+# = : assigning the value
+# == : checking if they are equal
---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,10 @@
+# performs the division to give the REMAINDER
+
+# question 2 - odd or even
+
+value = input("Number you want to check for odd/even: ")
+
+if int(value) % 2 == 0:
+    print(f"The number {value} is Even")
+else:
+    print(f"The number {value} id Odd")
