c79a5bb56cb1fd640223f26fad57db27aeb17b35
Your Name <demellab@sheridancollege.ca>
2025-06-08 16:28:49 -0400

    Logical Operators

--- Day 3/Logical Operators/task.py
+++ Day 3/Logical Operators/task.py
@@ -8,14 +8,20 @@ if height >= 120:
     if age < 12:
         bill = 5
         print("Child tickets are $5.")
-    elif age <= 18:
+    elif age>12 and age<18:
         bill = 7
         print("Youth tickets are $7.")
+
+    elif age>=45 and age<=55: # elif 45 >= age <=55 - less wordly approach
+        bill = 0
+        print("You can ride for free")
+
     else:
         bill = 12
         print("Adult tickets are $12.")
 
     wants_photo = input("Do you want a photo taken? Y or N. ")
+
     if wants_photo == "Y":
         bill += 3
 
