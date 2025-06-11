5c06b89d7dc1b989e9e8b8112676fc54ee0bf46c
Your Name <demellab@sheridancollege.ca>
2025-06-10 21:27:32 -0400

    Randomization

---
+++ Day 4/Random Module/my_module.py
@@ -0,0 +1,2 @@
+my_fav_number = 3.1415
+
---
+++ Day 4/Random Module/task.py
@@ -0,0 +1,19 @@
+import random #py module
+import my_module
+
+#module: each module is responsible for a diff task
+
+# print(random.randint(4, 6))
+
+# print(my_module.my_fav_number)
+
+random_no = random.random() #module, function
+# 0 <= number < 1 (semi open range)
+print(random_no)
+
+#extending the range
+random_no_10 = random.random() * 10
+print(random_no_10)
+
+#random float
+ran_uniform = random.uniform(1,10) #a <= number <=b
